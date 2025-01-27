3a2bd11235a2bbb117ebe1a88b83ed3835c10d77
Jeong Hun <jjh030212@naver.com>
2025-01-27 19:49:31 +0900

    20250127

---
+++ 0x0c_BACKTRACKING/1182.c
@@ -0,0 +1,23 @@
+// 1182 부분수열의 합
+#include <stdio.h>
+int n, s;
+int arr[25];
+int cnt=0;
+void func(int cur, int tot){
+    if(cur == n){
+        if(tot == s) cnt++;
+        return;
+    }
+    func(cur+1, tot);
+    func(cur+1, tot + arr[cur]);
+}
+int main(){
+    scanf("%d %d", &n, &s);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    func(0,0);
+    if(s==0)    cnt--;
+    printf("%d", cnt);
+    return 0;
+}
